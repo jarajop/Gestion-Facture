2755671f2d7dfa698c9099d00a58e31d9e34c087
jarajop <99683331+jarajop@users.noreply.github.com>
2023-12-18 11:51:30 +0000

    card_ciruit

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import Filtre from "./Filtre"
 function Card() {
-  return (
-    <main>
-    
-    <div className="row row-cols-2 row-cols-lg-4 g-2 g-lg-3 mx-0 p-0 my-5">
+  return  (
+    <main >
+    <div className="row row-cols-2 row-cols-lg-4 g-2 g-lg-4 mx-0 p-0 my-5">
       <Filtre />
       <Filtre />
       <Filtre />
+      <Filtre />
+
     </div>
 
     <div className="row row-cols-2 row-cols-lg-4 g-2 g-lg-3 mx-0 p-0 my-5">
       <Filtre />
       <Filtre />
       <Filtre />
+      <Filtre />
+
     </div>
   </main>  )
 }
--- src/pages/Filtre.jsx
+++ src/pages/Filtre.jsx
@@ -1,54 +1,51 @@
 import React from "react";
 import "../assets/style/filtre2.css"
-import Vehicule from "../assets/img/vue_aerienne_plage.jpg";
+import Monument from "../assets/img/monument.png";
 import { Icon } from "@iconify/react";
 
 function Filtre() {
   return (
-    <div id=" card_vehicule" className="pt-4 pl-5">
+    <div id="card_vehicule" className="pt-4 pl-5">
       <div className="row m-0 p-0  ">
-        <div className=" content_card col-lg-3  shadow-sm p-0 mb-0 bg-white">
+        <div className=" content_card col-lg-3  shadow-sm p-0 mb-0 bg-white ">
           <div className="content_card_vehicule d-flex justify-content-center ">
             {" "}
             <img
-              src={Vehicule}
+              src={Monument}
               alt=""
               className=" img_card_content "
               data-aos="zoom-in-up"
               data-aos-duration="1000"
             />
           </div>
-          <div className=" content_details">
+          <div className=" content_details ">
             <div className="content_text_vehicule">
-              <p className="text_vehicule "> Renault Dauster</p>
+              <p className="text_vehicule  fw-bold text-break pb-2"> Lorem ipsum dolor sit amet consectetur.</p>
             </div>
-            <div className="d-flex justify-content-start pt-3 pl-2 ">
-              <Icon
-                icon="mdi:user-group"
-                color="#f24873"
-                width="18"
-                height="18"
-              />
-              <div className="content_max justify-content-end px-3 ">
-                {" "}
-                Max 4 personnes
+           <div className=" d-flex justify-content-between p-3">
+           <div className="d-flex justify-content-start ">
+              <Icon icon="mingcute:star-fill" color="#f24873" width="24" height="24" />
+              <Icon icon="mingcute:star-fill" color="#f24873" width="24" height="24" />
+              <Icon icon="mingcute:star-fill" color="#f24873" width="24" height="24" />
+              <Icon icon="mingcute:star-fill" color="#f24873" width="24" height="24" />
+              <Icon icon="mingcute:star-fill" color="#f24873" width="24" height="24" />
+
+             
+            </div>
+           <div> <span className="content_avis fw-bold ">3 avis</span> </div>
+           </div>
+            <div className="d-flex justify-content-between p-3">
+             <div className="d-flex justify-content-start">
+             <Icon icon="entypo:time-slot" color="#f24873" width="28" height="28" />
+              <span className="content_hour"> 2h</span>
+             </div>
+               <div className="">
+                 <button className="button_vehicule border-0  ">
+                <span className="text_button  ">CHOISIR</span>
+              </button>
               </div>
             </div>
-            <div className="d-flex justify-content-start pt-1 pl-2">
-              <Icon
-                icon="fontisto:suitcase"
-                color="#f24873"
-                width="18"
-                height="18"
-              />{" "}
-              <div className="content_max  px-3"> 2(23kg) 2(10kg)</div>
-            </div>
-            <div className="d-flex justify-content-between pl-2 pt-4">
-              <p className=" content_max pt-1"> 60000 XOF</p>
-              <button className="button_vehicule border-0 mr-4 mb-2 ">
-                <span className="text_button  ">CHOISIR</span>
-              </button>{" "}
-            </div>
+            
           </div>
         </div>
       </div>
